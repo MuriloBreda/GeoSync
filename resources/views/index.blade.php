64f7ec6ba57de16d35667e3ef1c29e9714858723
Murilo <murilo.breda@aluno.senai.br>
2026-03-24 10:00:40 -0300

    TCC

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -65,7 +65,7 @@
     <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
             <a href="index.html" class="navbar-brand ml-lg-3">
-                <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Faster</h1>
+                <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Mais rápido</h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
@@ -92,7 +92,7 @@
             <h1 class="text-white display-3 mb-5">Serviços de logística</h1>
             <div class="mx-auto" style="width: 100%; max-width: 600px;">
                 <div class="input-group">
-                    <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="Tracking Id">
+                    <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="ID de rastreamento">
                     <div class="input-group-append">
                         <button class="btn btn-primary px-3">Rastrear</button>
                     </div>
@@ -189,7 +189,7 @@
                                 </select>
                             </div>
                             <div>
-                                <button class="btn btn-dark btn-block border-0 py-3" type="submit">Get A Quote</button>
+                                <button class="btn btn-dark btn-block border-0 py-3" type="submit">Obtenha uma cotação</button>
                             </div>
                         </form>
                     </div>
@@ -204,16 +204,16 @@
     <div class="container-fluid pt-5">
         <div class="container">
             <div class="text-center pb-2">
-                <h6 class="text-primary text-uppercase font-weight-bold">Our Services</h6>
-                <h1 class="mb-4">Best Logistic Services</h1>
+                <h6 class="text-primary text-uppercase font-weight-bold">Nossos serviços</h6>
+                <h1 class="mb-4">Melhores serviços logísticos</h1>
             </div>
             <div class="row pb-3">
                 <div class="col-lg-3 col-md-6 text-center mb-5">
                     <div class="d-flex align-items-center justify-content-center bg-primary mb-4 p-4">
                         <i class="fa fa-2x fa-plane text-dark pr-3"></i>
-                        <h6 class="text-white font-weight-medium m-0">Air Freight</h6>
+                        <h6 class="text-white font-weight-medium m-0">Frete Aéreo</h6>
                     </div>
-                    <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
+                    <p>Aliamos a solidez da nossa experiência à agilidade da inovação para entregar soluções que inspiram confiança. Com foco em resultados e transparência, transformamos seus desafios em projetos de sucesso, garantindo a excelência técnica que o seu negócio exige.</p>
                     <a class="border-bottom text-decoration-none" href="">Read More</a>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center mb-5">
@@ -221,7 +221,7 @@
                         <i class="fa fa-2x fa-ship text-dark pr-3"></i>
                         <h6 class="text-white font-weight-medium m-0">Ocean Freight</h6>
                     </div>
-                    <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
+                    <p>Aliamos a solidez da nossa experiência à agilidade da inovação para entregar soluções que inspiram confiança. Com foco em resultados e transparência, transformamos seus desafios em projetos de sucesso, garantindo a excelência técnica que o seu negócio exige.</p>
                     <a class="border-bottom text-decoration-none" href="">Read More</a>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center mb-5">
@@ -229,7 +229,7 @@
                         <i class="fa fa-2x fa-truck text-dark pr-3"></i>
                         <h6 class="text-white font-weight-medium m-0">Land Transport</h6>
                     </div>
-                    <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
+                    <p>Aliamos a solidez da nossa experiência à agilidade da inovação para entregar soluções que inspiram confiança. Com foco em resultados e transparência, transformamos seus desafios em projetos de sucesso, garantindo a excelência técnica que o seu negócio exige.</p>
                     <a class="border-bottom text-decoration-none" href="">Read More</a>
                 </div>
                 <div class="col-lg-3 col-md-6 text-center mb-5">
@@ -237,7 +237,7 @@
                         <i class="fa fa-2x fa-store text-dark pr-3"></i>
                         <h6 class="text-white font-weight-medium m-0">Cargo Storage</h6>
                     </div>
-                    <p>Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet diam sea est diam</p>
+                    <p>Aliamos a solidez da nossa experiência à agilidade da inovação para entregar soluções que inspiram confiança. Com foco em resultados e transparência, transformamos seus desafios em projetos de sucesso, garantindo a excelência técnica que o seu negócio exige.</p>
                     <a class="border-bottom text-decoration-none" href="">Read More</a>
                 </div>
             </div>
@@ -254,15 +254,15 @@
                     <img class="img-fluid w-100" src="img/feature.jpg" alt="">
                 </div>
                 <div class="col-lg-7 py-5 py-lg-0">
-                    <h6 class="text-primary text-uppercase font-weight-bold">Why Choose Us</h6>
-                    <h1 class="mb-4">Faster, Safe and Trusted Logistics Services</h1>
-                    <p class="mb-4">Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo</p>
+                    <h6 class="text-primary text-uppercase font-weight-bold">Por que nos escolher</h6>
+                    <h1 class="mb-4">Serviços de logística mais rápidos, seguros e confiáveis.</h1>
+                    <p class="mb-4">Nossa trajetória é pautada pela excelência técnica e pelo compromisso com resultados concretos. Unimos a experiência de nossos especialistas à inovação constante para transformar desafios complexos em soluções inteligentes. Acreditamos que a transparência e a dedicação são os pilares para construir parcerias sólidas e projetos de sucesso duradouro.</p>
                     <ul class="list-inline">
-                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>Best In Industry</h6>
-                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>Emergency Services</h6></li>
-                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>24/7 Customer Support</h6></li>
+                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>Melhor da indústria</h6>
+                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>Serviços de Emergência</h6></li>
+                        <li><h6><i class="far fa-dot-circle text-primary mr-3"></i>Suporte ao cliente 24 horas por dia, 7 dias por semana</h6></li>
                     </ul>
-                    <a href="" class="btn btn-primary mt-3 py-2 px-4">Learn More</a>
+                    <a href="" class="btn btn-primary mt-3 py-2 px-4">Saber mais</a>
                 </div>
             </div>
         </div>
