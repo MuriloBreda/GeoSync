b89811b7bae90f16fc1d7d4daaed80e0f2650652
Murilo <murilo.breda@aluno.senai.br>
2026-04-07 07:32:07 -0300

    TCC

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -63,35 +63,10 @@
 
 
     <!-- Navbar Start -->
-    <div class="container-fluid p-0">
-        <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
-            <a href="index.html" class="navbar-brand ml-lg-3">
-                <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>GeoSync</h1>
-            </a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
-                <div class="navbar-nav m-auto py-0">
-                    <a href="index.html" class="nav-item nav-link">Inicio</a>
-                    <a href="about.html" class="nav-item nav-link">Sobre</a>
-                    <a href="service.html" class="nav-item nav-link">Serviço</a>
-                    <a href="price.html" class="nav-item nav-link">Preço</a>
-                    <a href="contact.html" class="nav-item nav-link">Contato</a>
-                </div>
-                <a href="" class="btn btn-primary py-2 px-4 d-none d-lg-block">Solicite um Orçamento</a>
-            </div>
-        </nav>
-    </div>
     <!-- Navbar End -->
 
 
     <!-- Header Start -->
-    <div class="jumbotron jumbotron-fluid mb-5">
-        <div class="container text-center py-5">
-            <h1 class="text-white display-3">Serviço</h1>
-        </div>
-    </div>
     <!-- Header End -->
 
 
@@ -191,134 +166,10 @@
 
 
     <!--  Quote Request Start -->
-    <div class="container-fluid bg-secondary my-5">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-7 py-5 py-lg-0">
-                    <h6 class="text-primary text-uppercase font-weight-bold">Obter Uma Cotação</h6>
-                    <h1 class="mb-4">Solicite um Orçamento Grátis</h1>
-                    <p class="mb-4">A GeoSync otimiza o transporte de mercadorias com rastreamento em tempo real, garantindo mais controle, segurança e eficiência nas entregas.</p>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <h1 class="text-primary mb-2" data-toggle="counter-up">225</h1>
-                            <h6 class="font-weight-bold mb-4">Especialistas Qualificados</h6>
-                        </div>
-                        <div class="col-sm-4">
-                            <h1 class="text-primary mb-2" data-toggle="counter-up">1050</h1>
-                            <h6 class="font-weight-bold mb-4">Clientes Felizes</h6>
-                        </div>
-                        <div class="col-sm-4">
-                            <h1 class="text-primary mb-2" data-toggle="counter-up">2500</h1>
-                            <h6 class="font-weight-bold mb-4">Projeto Completos</h6>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="bg-primary py-5 px-4 px-sm-5">
-                        <form class="py-5">
-                            <div class="form-group">
-                                <input type="text" class="form-control border-0 p-4" placeholder="Seu Nome" required="required" />
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control border-0 p-4" placeholder="Seu Email" required="required" />
-                            </div>
-                            <div class="form-group">
-                                <select class="custom-select border-0 px-4" style="height: 47px;">
-                                    <option selected>Selecione Um Serviço</option>
-                                    <option value="1">Serviço 1</option>
-                                    <option value="2">Serviço 2</option>
-                                    <option value="3">Serviço 3</option>
-                                </select>
-                            </div>
-                            <div>
-                                <button class="btn btn-dark btn-block border-0 py-3" type="submit">Obter Uma Cotação</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Quote Request Start -->
 
 
     <!-- Testimonial Start -->
-    <div class="container-fluid py-5">
-        <div class="container">
-            <div class="text-center pb-2">
-                <h6 class="text-primary text-uppercase font-weight-bold">Comentários</h6>
-                <h1 class="mb-4">Nossos Clientes Dizem</h1>
-            </div>
-
-            <!-- Depoimentos de clientes sobre o sistema GeoSync -->
-<div class="owl-carousel testimonial-carousel">
-
-    <!-- Cliente 1 -->
-    <div class="position-relative bg-secondary p-4">
-        <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0;"></i>
-        <div class="d-flex align-items-center mb-3">
-            <img class="img-fluid rounded-circle" src="img/testimonial-1.jpg" style="width: 60px; height: 60px;" alt="">
-            <div class="ml-3">
-                <h6 class="font-weight-semi-bold m-0">Carlos Mendes</h6>
-                <small>- Gerente de Logística</small>
-            </div>
-        </div>
-        <p class="m-0">
-        O GeoSync facilitou muito o controle das nossas entregas. Agora conseguimos acompanhar nossos produtos em tempo real.
-        </p>
-    </div>
-
-    <!-- Cliente 2 -->
-    <div class="position-relative bg-secondary p-4">
-        <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0;"></i>
-        <div class="d-flex align-items-center mb-3">
-            <img class="img-fluid rounded-circle" src="img/testimonial-2.jpg" style="width: 60px; height: 60px;" alt="">
-            <div class="ml-3">
-                <h6 class="font-weight-semi-bold m-0">Fernanda Alves</h6>
-                <small>- Supervisora de Transporte</small>
-            </div>
-        </div>
-        <p class="m-0">
-        Com o GeoSync conseguimos monitorar todas as rotas dos motoristas.
-        O sistema é simples de usar e ajudou muito na organização da empresa.
-        </p>
-    </div>
-
-    <!-- Cliente 3 -->
-    <div class="position-relative bg-secondary p-4">
-        <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0;"></i>
-        <div class="d-flex align-items-center mb-3">
-            <img class="img-fluid rounded-circle" src="img/testimonial-3.jpg" style="width: 60px; height: 60px;" alt="">
-            <div class="ml-3">
-                <h6 class="font-weight-semi-bold m-0">Ricardo Souza</h6>
-                <small>- Empresário do Setor de Transportes</small>
-            </div>
-        </div>
-        <p class="m-0">
-        O rastreamento em tempo real melhorou muito nossa eficiência.
-        Hoje temos mais segurança e controle sobre nossas cargas.
-        </p>
-    </div>
-
-    <!-- Cliente 4 -->
-    <div class="position-relative bg-secondary p-4">
-        <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0;"></i>
-        <div class="d-flex align-items-center mb-3">
-            <img class="img-fluid rounded-circle" src="img/testimonial-4.jpg" style="width: 60px; height: 60px;" alt="">
-            <div class="ml-3">
-                <h6 class="font-weight-semi-bold m-0">Juliana Martins</h6>
-                <small>- Coordenadora de Operações</small>
-            </div>
-        </div>
-        <p class="m-0">
-        Excelente sistema para empresas que trabalham com transporte.
-        O GeoSync trouxe mais organização e controle para nossa logística.
-        </p>
-    </div>
-
-</div>
-</div>
-</div>
     <!-- Testimonial End -->
 
 
@@ -345,8 +196,6 @@
                         <div class="d-flex flex-column justify-content-start">
                             <a class="text-white mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Inicio</a>
                             <a class="text-white mb-2" href="about.html"><i class="fa fa-angle-right mr-2"></i>Sobre nós</a>
-                            <a class="text-white mb-2" href="service.html"><i class="fa fa-angle-right mr-2"></i>Nosso Serviço</a>
-                            <a class="text-white mb-2" href="price.html"><i class="fa fa-angle-right mr-2"></i>Plano de Preços</a>
                             <a class="text-white" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contate-nos</a>
                         </div>
                     </div>
