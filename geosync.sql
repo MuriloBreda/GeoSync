63d873553480d0be812a903378ba619727135e5c
Murilo <murilo.breda@aluno.senai.br>
2026-04-28 10:16:49 -0300

    28/04/26

---
+++ geosync.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE geosync;
+USE geosync;
+
+CREATE USER 'murilo'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON geosync.* TO 'murilo'@'%';
+FLUSH PRIVILEGES;
+
+-- USERS (usuario)
+CREATE TABLE users (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
+);
+
+ALTER TABLE users ADD foto VARCHAR(255) NULL;
+
+-- REMESSAS
+CREATE TABLE remessas (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    codigo_rastreio VARCHAR(100),
+    origem VARCHAR(100),
+    destino VARCHAR(100),
+    tipo_carga VARCHAR(100),
+    peso DECIMAL(10,2),
+    previsao_entrega DATE,
+    status VARCHAR(50),
+
+    user_id BIGINT UNSIGNED,
+
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+-- ALERTAS
+CREATE TABLE alertas (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tipo_alerta VARCHAR(100),
+    descricao VARCHAR(255),
+
+    remessa_id BIGINT UNSIGNED,
+
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+
+    FOREIGN KEY (remessa_id) REFERENCES remessas(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+-- LOCALIZACOES
+CREATE TABLE localizacoes (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    latitude DECIMAL(10,8),
+    longitude DECIMAL(11,8),
+
+    remessa_id BIGINT UNSIGNED,
+
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+
+    FOREIGN KEY (remessa_id) REFERENCES remessas(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+select * from users;
